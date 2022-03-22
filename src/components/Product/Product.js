df8b3160fd05e75a86baeb5361c9434bbca3a542
AbuZobayed <zobayed.ict05@gmail.com>
2022-03-22 20:54:47 +0600

    AddToCard Working

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -4,6 +4,8 @@ import './Product.css'
 const Product = (props) => {
     
     const {name , img , seller , price , ratings} = props.product;
+
+    console.log(props);
     return (
         <div className='product'>
             <img src={img} alt="product-img" />
@@ -13,7 +15,9 @@ const Product = (props) => {
             <p><small>Seller:{seller}</small></p>
             <p><small>Ratings:{ratings} stars</small></p>
             </div>
-            <button className='btn-card'>
+            <button 
+            onClick={() => props.handleAddToCard(props.product)}
+             className='btn-card'>
                 <p>Add to Card</p>
             </button>
         </div>
