513301ba44be92d024d7668fdf272806b34e16c7
AbuZobayed <zobayed.ict05@gmail.com>
2022-03-22 22:13:16 +0600

    change code

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -7,20 +7,23 @@ const Product = (props) => {
     const {product,handleAddToCard} = props;
     const {name , img , seller , price , ratings} = product;
 
-    console.log(props);
+    
     return (
+
         <div className='product'>
             <img src={img} alt="product-img" />
             <div className='product-info'>
             <p className='product-name'>{name}</p>
             <p>Price: ${price}</p>
+
             <p><small>Seller:{seller}</small></p>
+            
             <p><small>Ratings:{ratings} stars</small></p>
             </div>
             <button 
             onClick={() => handleAddToCard(product)}
              className='btn-card'>
-                <p>Add to Card</p>
+                <p>Add Card</p>
                 <FontAwesomeIcon icon={faCoffee}></FontAwesomeIcon>
             </button>
         </div>
